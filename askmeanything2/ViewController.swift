db88c72c0999206081203f811413f2fbaf0a7c32
afoong2003 <afoong2003@gmail.com>
2021-07-20 19:59:06 -0500

    Switch

--- askmeanything2/ViewController.swift
+++ askmeanything2/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        magicEightBall()
+    }
+    func magicEightBall() {
+        let randomNum = Int.random(in: 0...4)
+        
+        switch randomNum {
+        case 0:
+            imageView.image = UIImage(named: "food1")
+        case 1:
+            imageView.image = UIImage(named: "food2")
+        case 2:
+            imageView.image = UIImage(named: "food3")
+        case 3:
+            imageView.image = UIImage(named: "food4")
+        case 4:
+            imageView.image = UIImage(named: "food5")
+        default:
+            print("Click on Button")
+        
+        
+        }
+        
+    }
 
 }
 
